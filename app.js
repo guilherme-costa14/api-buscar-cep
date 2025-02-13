764337a081d28011f4754c4430f95011fd98371e
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-13 17:08:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//armazena o cep digitado para fazer a busca
 let botao = document.getElementById("botao-buscar-cep")
 botao.addEventListener("click", function(){
     let cep = document.getElementById("input-cep").value
     fnBuscarCep(cep)
 })
 
+//faz a consulta na api com o cep que está armazenado na variável cep
 function fnBuscarCep(cep){
     const url = `https://cep.awesomeapi.com.br/json/${cep}`;
     fetch(url)
@@ -18,6 +20,7 @@ function fnBuscarCep(cep){
     })
 }
 
+//retornar as informações para exibir na tela
 function fnExibirInformacoes(rua, bairro, cidade, estado){
     document.getElementById("rua").value = rua
     document.getElementById("bairro").value = bairro
